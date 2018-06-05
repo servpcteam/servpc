b827580740e9add1b4e29fa88e9608ac7488dc8d
Wojtek_M <35421818+WojciechMalicki@users.noreply.github.com>
2018-06-05 22:59:13 +0200

    dodana kolumna Producent

--- userpanel.php
+++ userpanel.php
@@ -37,6 +37,7 @@ if (!isset($_SESSION['loggedIn'])) {
   <thead>
     <tr>
       <th scope="col">Rodzaj</th>
+      <th scope="col">Producent</th>
       <th scope="col">Numer seryjny</th>
       <th scope="col">Opis</th>
       <th scope="col">Statut</th>
@@ -47,9 +48,10 @@ if (!isset($_SESSION['loggedIn'])) {
 	<?php
 		$idKlient = $_SESSION['idKlient'];
 		$db = new DBconnector();
-		$orders = $db->get_array("SELECT zgloszenie.ID_Zgloszenie, sprzet.SerialNumber, rodzaj.Nazwa, zgloszenie.Opis, status_z.Status FROM ((sprzet INNER JOIN zgloszenie ON sprzet.ID_Sprzet = zgloszenie.SPRZET_ID_Sprzet) INNER JOIN status_z ON status_z.ID_Status = zgloszenie.STATUS_Z_ID_Status) INNER JOIN rodzaj ON rodzaj.ID_Rodzaj = sprzet.RODZAJ_ID_Rodzaj WHERE zgloszenie.KLIENT_ID_Klient = $idKlient");
+		$orders = $db->get_array("SELECT zgloszenie.ID_Zgloszenie, sprzet.Producent, sprzet.SerialNumber, rodzaj.Nazwa, zgloszenie.Opis, status_z.Status FROM ((sprzet INNER JOIN zgloszenie ON sprzet.ID_Sprzet = zgloszenie.SPRZET_ID_Sprzet) INNER JOIN status_z ON status_z.ID_Status = zgloszenie.STATUS_Z_ID_Status) INNER JOIN rodzaj ON rodzaj.ID_Rodzaj = sprzet.RODZAJ_ID_Rodzaj WHERE zgloszenie.KLIENT_ID_Klient = $idKlient");
     foreach($orders as $order) {
 			echo '<tr>'.'<td>'.$order['Nazwa'].'</td>'
+      .'<td>'.$order['Producent'].'</td>'
 			.'<td>'.$order['SerialNumber'].'</td>'
 			.'<td>'.$order['Opis'].'</td>'
 			.'<td>'.$order['Status'].'</td>'
