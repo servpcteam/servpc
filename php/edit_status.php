222111cd54c35a96794deb0e40e5ced899792883
Wojtek_M <35421818+WojciechMalicki@users.noreply.github.com>
2018-06-06 11:32:06 +0200

    poprawa edit_status.php

--- php/edit_status.php
+++ php/edit_status.php
@@ -6,7 +6,6 @@ $id = $_POST['id'];
 $db = new DBconnector();
 $id_status_array = $db->get_array("SELECT STATUS_Z_ID_Status FROM Zgloszenie WHERE ID_Zgloszenie=$id");
 $id_status = ($id_status_array[0]['STATUS_Z_ID_Status'] % 3) + 1;
-var_dump($id_status);
 $db->execute("UPDATE Zgloszenie SET STATUS_Z_ID_Status = $id_status WHERE ID_Zgloszenie=$id");
 
 echo "Zmieniono status";
